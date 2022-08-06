d85f628be91a715672850f8995a69f9199cfe611
obarker <oliverbarker@aarsleff.co.uk>
2022-08-06 16:45:05 +0100

    basic fin fetch done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::env;
+use exitfailure::ExitFailure;
+use reqwest::Url;
+use serde_derive::{Deserialize, Serialize};
+
 
 fn verbose() {
     let name = "USER";
@@ -13,19 +17,41 @@ fn short() {
 }
 
 
-fn main() {
+#[derive(Serialize, Deserialize, Debug)]
+struct CompanyQuote {
+    c: f64,
+    h: f64,
+    l: f64,
+    o: f64,
+    pc: f64,
+    t: i128
+}
+
+impl CompanyQuote {
+    async fn get(symbol: &String, api_key: &String) -> Result<Self, ExitFailure> {
+        let url = format!("https://finnhub.io/api/v1/quote?symbol={}&token={}", symbol, api_key);
+        let url = Url::parse(&*url)?;
+        let res = reqwest::get(url).await?.json::<CompanyQuote>().await?;
+        Ok(res)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), ExitFailure> {
+    let api_key = "cbn7vjiad3ifu7vkomog".to_string();
     let args: Vec<String> = env::args().collect();
-    let mut company: String = "APPL".to_string();
+    let mut symbol: String = "AAPL".to_string();
 
     if args.len() < 2 {
-        println!("No symbol has been specified, so using AAPL as default");
+        println!("Since you didn't specify a company symbol, it has defaulted to AAPL.");
+    } else {
+        symbol = args[1].clone();
     }
-    else {
-        company = args[1].clone();
-    }
-    println!("{:?}", args);    
-    println!("Hello, world!");
-    println!("{}", company);
-    
-    
+
+    let res = CompanyQuote::get(&symbol, &api_key).await?;
+    println!("{}'s current stock price: {}", symbol, res.c);
+
+    Ok(())
 }
+
+
